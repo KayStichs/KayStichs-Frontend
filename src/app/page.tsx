2b2938551c892d82430fe6334194b18d690f2175
MicD746 <michaeldanladi746@gmail.com>
2026-04-28 16:53:49 +0100

    Revert "feat: add value proposition landing section for issue #140"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from "@/components/landing/hero";
-import ValuePreposition from "@/components/landing/value-preposition";
 import WhyKayStichs from "@/components/landing/why-kaystichs";
 import Testimonial from "@/components/landing/testimonial";
 
@@ -7,7 +6,6 @@ const Home = () => {
   return (
     <main>
       <Hero />
-      <ValuePreposition />
       <WhyKayStichs />
       <Testimonial />
     </main>
