f7638445a3e3dfe04ae801587372b3009040f03c
MicD746 <michaeldanladi746@gmail.com>
2026-04-28 12:08:43 +0100

    Added why KayStichs landing section, both desktop and mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/landing/hero";
+import WhyKayStichs from "@/components/landing/why-kaystichs";
 
 const Home = () => {
   return (
     <main>
       <Hero />
+      <WhyKayStichs />
     </main>
   );
 };
