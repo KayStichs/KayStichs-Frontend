6ff5097112b3dafa4a826c5a34917d3ed8c7403f
MicD746 <michaeldanladi746@gmail.com>
2026-04-28 09:53:37 -0700

    Added Footer to App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Hero from "@/components/landing/hero";
 import WhyKayStichs from "@/components/landing/why-kaystichs";
 import Testimonial from "@/components/landing/testimonial";
-
+import Footer from "@/components/landing/footer";
 const Home = () => {
   return (
     <main>
       <Hero />
       <WhyKayStichs />
       <Testimonial />
+      <Footer/>
     </main>
   );
 };
