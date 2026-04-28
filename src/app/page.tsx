466afe2642d409d414e77d17b9e32e79b86a5afd
MicD746 <michaeldanladi746@gmail.com>
2026-04-28 13:42:02 +0100

    #138 [Frontend] Build the UI of the Testimonial section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/landing/hero";
 import WhyKayStichs from "@/components/landing/why-kaystichs";
+import Testimonial from "@/components/landing/testimonial";
 
 const Home = () => {
   return (
     <main>
       <Hero />
       <WhyKayStichs />
+      <Testimonial />
     </main>
   );
 };
