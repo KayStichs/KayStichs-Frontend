0b6f9ec2af7b3a1b87d3aea5ceed63b0d8612734
MicD746 <michaeldanladi746@gmail.com>
2026-04-28 23:47:43 +0100

    feat:add value preposition component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/landing/hero";
 import WhyKayStichs from "@/components/landing/why-kaystichs";
 import Testimonial from "@/components/landing/testimonial";
+import ValueProposition from "@/components/landing/value-preposition";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Hero />
       <WhyKayStichs />
       <Testimonial />
+      <ValueProposition/>
     </main>
   );
 };
